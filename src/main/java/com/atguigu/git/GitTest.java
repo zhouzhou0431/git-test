2e4508885493ecbcc2c33d7521915146aa32dc31
zhouzhou0431 <109527032+zhouzhou0431@users.noreply.github.com>
2022-12-25 23:59:16 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix");
         System.out.println("hello push");
+        System.out.println("hello pull");
     }
 }
